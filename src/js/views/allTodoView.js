fd22bb2f60c2c449c046036654bb2081b8169076
Abdulrahaman Ridwan Ayinla <abdulrahamanr657@gmail.com>
2025-08-07 16:17:12 -0700

    completed the todo app

--- src/js/views/allTodoView.js
+++ src/js/views/allTodoView.js
@@ -1,3 +1,5 @@
+import iconDelete from '/src/images/icon-cross.svg'
+
 export default class AllTodoView {
     _parentElement = document.getElementById('todo-list')
      _todoParentEl = document.querySelector('.todo-div')
@@ -25,7 +27,7 @@ export default class AllTodoView {
         <img  class="absolute left-1 top-2 opacity-0 peer-checked:opacity-100 transition-opacity" src="./src/images/icon-check.svg" alt="icon-check">
         <span class="todo-text ${todo.completed ? 'line-through' : ''} ml-2  ${todo.completed ? 'text-gray-200' : ''} dark:${todo.completed ? 'text-gray-500' : ''} transition-colors duration-300"> ${todo.text}</span>
         </label>
-        <img class="delete-btn cursor-pointer" src="/src/images/icon-cross.svg" alt="icon-delete" />
+        <img class="delete-btn cursor-pointer" src="${iconDelete}" alt="icon-delete" />
         </div>
       `
       this._parentElement.insertAdjacentHTML('beforeend', markup);
